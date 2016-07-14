52ca6a9b606fe8cf75adf0575039c9f386ec3539
LFer <lferreira@datamatic.com.uy>
2016-07-14 17:29:38 -0300

    first commit

---
+++ __openerp__.py
@@ -0,0 +1,20 @@
+{
+    'name': 'Adaptacion Productos',
+    'version': '1.0',
+    'depends': ['base', 'product'],
+    'author': 'Felipe Ferreira',
+    'website': 'http://www.datamatic.com.uy',
+    'category': 'Datamatic',
+    'summary': 'Product extensions',
+    'description': """
+    Adds standard price (cost) to product template tree view.
+    """,
+    'data': [
+        'views/dtm_product_view.xml',
+    ],
+    'demo': [
+    ],
+    'auto_install': False,
+    'installable': True,
+    'application': True
+}
